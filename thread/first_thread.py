1041637768c9d3396a639612fbfe6e580d334011
gaozx <gaozx.fnst@cn.fujitsu.com>
2014-12-04 09:14:17 +0800

    **

--- thread/first_thread.py
+++ thread/first_thread.py
@@ -1,18 +1,23 @@
 import time
 import threading
 
+ll = []
+for x in range(10000):
+	ll.append(x)
+
+
+
 def loop():
 	print 'thread %s is running...' % threading.current_thread().name
 	n = 0
-	while n < 10:
+	while n < 5:
 		n = n + 1
 		print 'thread %s >>>> %s' % (threading.current_thread().name, n)
-		time.sleep(2)
+		time.sleep(5)
 	print 'thread %s is dead' % threading.current_thread().name
 
 print 'thread %s is running...' % threading.current_thread().name
 t = threading.Thread(target=loop, name='LoopThread')
 t.start()
 t.join()
-
-
+print 'thread %s end...' % threading.current_thread().name
